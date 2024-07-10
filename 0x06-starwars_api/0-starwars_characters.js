59a51c845e5f0e52f0039196149559e3006d5300
Pibo Shongwe <piboman@yahoo.com>
2024-07-10 19:30:56 +0000

    u

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.io/api';
+
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersArray = JSON.parse(body).characters;
+    const characterNames = charactersArray.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseError, __, characterRequest) => {
+          if (promiseError) {
+            reject(promiseError);
+          }
+          resolve(JSON.parse(characterRequest).name);
+        });
+      }));
+
+    Promise.all(characterNames)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErrors => console.log(allErrors));
+  });
+}
